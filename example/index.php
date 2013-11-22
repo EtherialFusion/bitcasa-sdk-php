0812f036cc56d172992a9380c865b79d661999b7
Roger Allan <rallan@bitcasa.com>
2013-11-21 16:15:09 -0800

    revert past fix

--- example/index.php
+++ example/index.php
@@ -4,5 +4,5 @@ include_once("config.php");
 ?>
 <html><head><title>Test Login</title></head><body>
 <h1>Bitcasa PHP SDK Login</h1>
-<a href="<?= BitcasaClient::authorize(OAUTH_CLIENTID, ($_SERVER["SERVER_PORT"] == 80 ? "http://" : "https://" ).$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"]."example.php"); ?>" > Login</a>
+<a href="<?= BitcasaClient::authorize(OAUTH_CLIENTID, ($_SERVER["SERVER_PORT"] == 80 ? "http://" : "https://" ) . $_SERVER["SERVER_NAME"] . ($_SERVER["REQUEST_URI"]) . "/auth.php"); ?>" > Login</a>
 </body></html>
